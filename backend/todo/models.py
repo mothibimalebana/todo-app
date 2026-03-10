2f11ecfba97c31cb84dbb83c076663ed2382a71f
mothibimalebana <mothibi.mm12@gmail.com>
2026-03-10 12:40:46 +0200

    set time zone to africa/johannnesburg

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    user = User
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
