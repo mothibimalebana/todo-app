490f3fb183578437010a9a7fc2998f4b4deabaf9
mothibimalebana <mothibi.mm12@gmail.com>
2026-03-11 14:45:52 +0200

    sign up api is complete

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import TaskList, TaskDetail, TaskCompleteUpdate
+from api.views import TaskList, TaskDetail, TaskCompleteUpdate, signup
 
 urlpatterns = [
     path('tasks/', TaskList.as_view()),
     path('tasks/<int:pk>', TaskDetail.as_view()),
     path('tasks/<int:pk>/complete', TaskCompleteUpdate.as_view()),
-    path('signup', )
+    path('signup', signup)
 ]
