b5cb9821f9d01e1537c1352f20c777d174129091
mothibimalebana <mothibi.mm12@gmail.com>
2026-03-12 09:17:51 +0200

    configured router in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Login from './components/login.tsx'
+import SignUp from './components/signup.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/sign-in",
+    element: <SignUp/>
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
